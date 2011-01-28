e4e09ace8ba63660169d15e796080808bf6a0476
Tony Brewer <tonybrewer@wildfirecybernetics.com>
2011-01-28 09:55:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChapterAdmin::Application.config.secret_token = '424941b1e058b61528c78288b912764df90d439f10d618b97fe5609d3741e75761aa1b66b77b4e60868dfdf50316f297bea901872ec89e66714dee580fb9e1a4'
