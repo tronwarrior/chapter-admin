189f50f39174e57a1322cb6b7a0aa9eb080ff32f
Tony Brewer <acbrewer@asu.edu>
2011-06-03 16:47:10 -0700

    respos cleanup

--- calendar_date_select/ruby/1.9.1/gems/dynamic_default_scoping-0.0.4/spec/support/matchers.rb
+++
@@ -1,7 +0,0 @@
-RSpec::Matchers.define :have_valid_locale do |*args|
-  locales = Array.wrap(args[0] || I18n.locale).map &:to_s
-
-  match do |*records|
-    records.flatten.all? { |record| locales.include? record.locale }
-  end
-end
--- calendar_date_select/ruby/1.9.1/gems/dynamic_default_scoping-0.0.4/spec/tables/posts.rb
+++
@@ -1,4 +0,0 @@
-create_table 'posts' do |table|
-  table.string :locale
-  table.timestamps
-end
